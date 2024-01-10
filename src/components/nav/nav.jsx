f7a3ebbf22b47f4a0feb3b383c0f0ab85da4666c
Vision Jain <visionjain118@gmail.com>
2024-01-10 20:00:09 +0530

    tail

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <div className='bg-black'>
+                navigation
+            </div>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Nav from '@/components/nav/nav'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default index
